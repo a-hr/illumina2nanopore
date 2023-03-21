14da088d0343ed9aa521a8c6a7d95512013c6acf
a-hr <heerreroalvaro@gmail.com>
2023-03-21 23:52:26 +0100

    small update

--- launch_cluster.sh
+++ launch_cluster.sh
@@ -5,8 +5,9 @@
 
 # load dependencies
 module load Singularity Nextflow
+
 # limit the resources of the JVM
 export NXF_OPTS="-Xms500M -Xmx8G"
-# export NXF_OPTS="-Dleveldb.mmap=false"
+
 # launch the main process
 nextflow run main.nf -resume -profile cluster -params-file input_params.yaml
